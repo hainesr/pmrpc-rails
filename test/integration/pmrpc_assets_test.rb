2b8551c996c2c1c8de203b9673cb9e058bc37ee9
Robert Haines <rhaines@manchester.ac.uk>
2013-11-07 17:26:35 +0000

    Actually assert that pmrpc.js can be served.

--- test/integration/pmrpc_assets_test.rb
+++ test/integration/pmrpc_assets_test.rb
@@ -3,5 +3,6 @@ require 'test_helper'
 class PmrpcAssetsTest < ActionDispatch::IntegrationTest
   test "pmrpc available to the main app" do
     get "/assets/pmrpc.js"
+    assert :success
   end
 end
